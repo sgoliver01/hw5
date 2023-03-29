c3323c1d26cbf55a4d0b2fdefb16393515c3a762
sgoliver01 <109550283+sgoliver01@users.noreply.github.com>
2023-03-29 18:16:46 -0400

    core task 4 kinda working but its still slow???

--- RayTracer.js
+++ RayTracer.js
@@ -62,6 +62,7 @@ export class RayTracer {
                 this.putPixel(row,col,color.x, color.y, color.z)
             }
         }
+        //at some point need to call the AABBNode constructor
     }
     
 
@@ -88,13 +89,6 @@ export class RayTracer {
         */
         // TODO
         
-        //oldway
-//           const x = (col-.5*this.image.width)/this.image.width; // goes from -0.5 (left) to 0.5 (right)
-//        const y = (.5*this.image.height-row)/this.image.height; // goes from -0.5 (bottom) to 0.5 (top)
-//        const ray = new Ray(new Vector3(0,0,0),new Vector3(x, y, -1)); 
-////        return ray
-  
-   
      //   new with camera positioning
         const A = e //camera pt
         const d = this.scene.f_imageplaneDistance
@@ -107,7 +101,6 @@ export class RayTracer {
         const first_addition = vectorSum(first_term, second)
         
         const topLeft = vectorDifference(first_addition, third)
-        //console.log(topLeft)
         
         //compute ray for every pixel
         const squareHeight = this.scene.f_imageplaneHeight/ this.scene.i_height
@@ -118,7 +111,6 @@ export class RayTracer {
         const first_pixel_third_term = vectorScaled(u,(.5*squareWidth))
         const dif = vectorDifference(topLeft, first_pixel_second_term)
         const firstPixel = vectorSum(dif, first_pixel_third_term)
-        //console.log(firstPixel)
         
         const B_second_term = vectorScaled(v,(row*squareHeight))
         const B_third_term =  vectorScaled(u,(col*squareWidth))
@@ -128,10 +120,7 @@ export class RayTracer {
         
         const direction = vectorDifference(B,A)
         const ray = new Ray(A,direction)
-       // console.log(ray)
-        
-        
-        
+
         return ray
     }
     
@@ -140,8 +129,8 @@ export class RayTracer {
         Determine the color of the given ray based on this.scene.
         */
         // TODO
-        
-        const hits = ray.allHits(this.scene.a_geometries);
+        const newNode = new AABBNode(this.scene.a_geometries)
+        const hits = ray.hitsOfNode(newNode);
         
         let minHit = {t: Infinity}
         for (const h of hits) {
@@ -205,7 +194,7 @@ export class RayTracer {
         const shadowRay_dir = vectorDifference(light_source.v3_position, point)  
         const shadowRay = new Ray(point, shadowRay_dir)
         
-        const shadow_hit = shadowRay.allHits(this.scene.a_geometries)
+        const shadow_hit = shadowRay.hitsOfNode(new AABBNode(this.scene.a_geometries))
             
         for (const hit of shadow_hit) { 
 
@@ -367,11 +356,9 @@ class Ray {
         else if (g.s_type === 'cylinder') {
             return this.hitCylinder(g);
         }
-        
         else if (g.s_type === 'triangle') {
             return this.hitTriangle(g);
         }
-        
         else {
             console.error("Shape of type " + g.s_type + " is not supported");
         }
@@ -424,86 +411,6 @@ class Ray {
         const hit = new HitRecord(this, t, pt, g, g.normal)
         return [hit];
         
-        
-    }
-    
-    
-    getM(a,b,c,d,e,f,g,h,i){
-        return (a*(e*i-h*f)) + (b*(g*f-d*i)) + (c*(d*h - e*g))
-    }
-    
-    beta(a,b,c,d,e,f,g,h,i,j,k,l){
-        return (j*(e*i - h*f)) + (k*(g*f - d*i)) + (l*(d*h-e*g))
-        
-    }
-    
-    gamma(a,b,c,d,e,f,g,h,i,j,k,l){
-        return (i*(a*k-j*b)) + (h*(j*c-a*l)) + (g*(b*l-k*c))
-    }
-    
-    getT(a,b,c,d,e,f,g,h,i,j,k,l){
-        
-        let returnMe = f*(a*k-j*b) + e*(j*c-a*l) + d*(b*l-k*c)
-        returnMe = returnMe * -1
-        return returnMe
-    }
-    
-    
-    
-    
-     hitTriangle(g){
-        
-        const a = g.v3_pt0;
-        const b = g.v3_pt1;
-        const c = g.v3_pt2;
-        
-        const e = this.start
-        const v = this.dir 
-        
-        
-        const edge1 = vectorDifference(b,a)
-        const edge2 = vectorDifference(b,c)
-        
-        const norm = edge2.crossProduct(edge1).normalize()
-        
-        //direction = v
-        //e is start 
-        
-        const m = this.getM(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z)
-        
-        let beta = this.beta(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z, e.x-a.x, e.y-a.y, e.z-a.z)
-        
-        beta = beta/m
-         
-        let gam = this.gamma(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z, e.x-a.x, e.y-a.y, e.z-a.z)
-        
-        gam = gam/m
-         
-        let t = this.getT(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z, e.x-a.x, e.y-a.y, e.z-a.z)
-        
-        t = t/m
-        const pt = this.tToPt(t)
-        
-        const alpha = 1-beta-gam
-        
-    
-        if (alpha<0 || alpha>1){
-            return []
-        }
-        
-        if (beta<0 || beta>1){
-            return []
-        }
-         
-        if (gam<0 || gam>1){
-            return []
-        }
-         
-        const hit =  new HitRecord(this, t, pt, g, norm)
-        return [hit]
-        
-        
-   
         
     }
 
@@ -643,7 +550,7 @@ class Ray {
             //whap the values if the second is bigger than the second
              if (((boxZ[0] - e.z)/ v.z) > ((boxZ[1] - e.z)/v.z)) {
                  intervalZ = [(boxZ[1] - e.z)/v.z, (boxZ[0] - e.z)/ v.z]
-                }
+            }
         }
         
         
@@ -654,7 +561,6 @@ class Ray {
         const min_end = (Math.min(intervalX[1], intervalY[1], intervalZ[1]))
         
         
-     //   console.log(max_start, min_end)
         //if intervals overlap
         if (max_start <= min_end) {
             const t1 = max_start
@@ -705,43 +611,153 @@ class Ray {
             else if (Math.abs(pt2.z - (g.v3_minPt.z +g.v3_dim.z))< EPSILON ) {
                 normal2 = new Vector3(0,0,1)
             }
-            //console.log(normal1)
-            
-           // console.log(normal2)
-
+     
 
             const hit1 = new HitRecord(this, t1, pt1, g, normal1)
             const hit2 = new HitRecord(this, t2, pt2, g, normal2)
-            
-            
-      
+
             return [hit1, hit2]
         }
         else {
             
             return []
         }
+    }
+    
+     getM(a,b,c,d,e,f,g,h,i){
+        return (a*(e*i-h*f)) + (b*(g*f-d*i)) + (c*(d*h - e*g))
+    }
+    
+    beta(a,b,c,d,e,f,g,h,i,j,k,l){
+        return (j*(e*i - h*f)) + (k*(g*f - d*i)) + (l*(d*h-e*g))
         
-
+    }
+    
+    gamma(a,b,c,d,e,f,g,h,i,j,k,l){
+        return (i*(a*k-j*b)) + (h*(j*c-a*l)) + (g*(b*l-k*c))
+    }
+    
+    getT(a,b,c,d,e,f,g,h,i,j,k,l){
+        
+        let returnMe = f*(a*k-j*b) + e*(j*c-a*l) + d*(b*l-k*c)
+        returnMe = returnMe * -1
+        return returnMe
     }
     
     
-//    hitCylinder(g) {
-//        const center = g3.v3_center
-//        const height = g.f_height
-//        const radius = g.f_radius
-//        
-//        const testSphere = {
-//            v3_center: new Vector3(center.x, 0, center.z)
-//            f_radius: radius,
-//        }
-//        const testRay = new Ray(new Vector3(this.start.x, 0, this.start.z, new Vector3(this.dir.x, 0, this.dir.z)))
-//        const hits = testRay.hitSphere(testSphere)
-//        const ret = []
-//        for (const h of hits) {
-//            const pt = this.tToPt(h.t)
-//        }
-//    }
+     hitTriangle(g){
+        
+        const a = g.v3_pt0;
+        const b = g.v3_pt1;
+        const c = g.v3_pt2;
+        
+        const e = this.start
+        const v = this.dir 
+        
+        
+        const edge1 = vectorDifference(b,a)
+        const edge2 = vectorDifference(b,c)
+        
+        const norm = edge2.crossProduct(edge1).normalize()
+        
+        //direction = v
+        //e is start 
+        
+        const m = this.getM(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z)
+        
+        let beta = this.beta(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z, e.x-a.x, e.y-a.y, e.z-a.z)
+        
+        beta = beta/m
+         
+        let gam = this.gamma(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z, e.x-a.x, e.y-a.y, e.z-a.z)
+        
+        gam = gam/m
+         
+        let t = this.getT(b.x-a.x, b.y-a.y, b.z-a.z, c.x-a.x, c.y-a.y, c.z-a.z, -v.x, -v.y, -v.z, e.x-a.x, e.y-a.y, e.z-a.z)
+        
+        t = t/m
+        const pt = this.tToPt(t)
+        
+        const alpha = 1-beta-gam
+        
+    
+        if (alpha<0 || alpha>1){
+            return []
+        }
+        
+        if (beta<0 || beta>1){
+            return []
+        }
+         
+        if (gam<0 || gam>1){
+            return []
+        }
+         
+        const hit =  new HitRecord(this, t, pt, g, norm)
+        return [hit]
+   
+    }
+        
+        
+    hitsOfNode(node) {
+        //return list of hits between ray and objects contained at or below the given AABB-tree node
+        
+        
+        
+        //check if ray hits node's bounding box
+        const hits = this.hitBox(node.box)
+        //if ray doesn't hit node.boundingbox
+        if (hits.length == 0) {
+            return [] 
+        }
+        if (node.left == null && node.right == null) {
+            return this.allHits(node.geometries)    
+        }
+        
+        const leftHits = this.hitsOfNode(node.left)
+        const rightHits = this.hitsOfNode(node.right)
+        
+        const fullHits = leftHits.concat(rightHits)
+        
+        return fullHits
+        
+        }
+
+    
+    hitCylinder(g) {
+        const center = g3.v3_center
+        const height = g.f_height
+        const radius = g.f_radius
+        
+        const testSphere = {
+            v3_center: new Vector3(center.x, 0, center.z),
+            f_radius: radius,
+        }
+        const testRay = new Ray(new Vector3(this.start.x, 0, this.start.z, new Vector3(this.dir.x, 0, this.dir.z)))
+        const hits = testRay.hitSphere(testSphere)
+        const ret = []
+        for (const h of hits) {
+            const pt = this.tToPt(h.t)
+            if (center.y - height/2 < pt.y && pt.y < center.y + height/2) {
+                h.struckGeometry = g
+                h.pt = pt
+                h.ray = this
+                ret.push(h)
+            }
+        }
+        for (const m of [-1,1]) {
+            const capCenter = new Vector3(center.x, center.y + m*height/2, center.z)
+            const normal = new Vector3(0,m,0)
+            const rhs = normal.dotProduct(capCenter)
+            const t = (rhs - this.start.dotProduct(normal))/this.dir.dotProduct(normal)
+            const hit = new HitRecord(this, t, this.tToPt(t), g, normal)
+            const ptToAxis = vectorDifference(capCenter, hit.pt)
+            if (ptToAxis.norm() < radius) {
+                ret.push(hit)
+            }
+        }
+        return ret
+    }
 }
 
 class HitRecord {
@@ -752,4 +768,172 @@ class HitRecord {
         this.struckGeometry = struckGeometry; // object that was hit
         this.normal = normal; // normal vector of struckGeometry at pt
     }
+}
+
+class AABBNode {
+    constructor(g) {
+        this.geometries = g
+        const firstNode = this.boxAround(g[0])
+        const secondNode = this.boxAround(g[1])
+        var newBox = this.boxAround2(firstNode, secondNode)
+        if (g.length > 3) {
+            for (var i = 3; i < g.length; i++) {
+                const latestBox = this.boxAround2(this.boxAround(g[i]), newBox)
+                newBox = latestBox
+            }
+        }
+        this.box = newBox
+        if (g.length <= 3) {
+            //make a leaf
+            this.left = null
+            this.right = null
+            this.box = newBox
+            return
+        }
+        const sortedGeometries = g.sort((a,b) => this.boxAround(a).v3_minPt.x - this.boxAround(b).v3_minPt.x);
+        
+        const leftHalf = sortedGeometries.slice(0, sortedGeometries.length/2)
+        const rightHalf = sortedGeometries.slice(sortedGeometries.length/2, sortedGeometries.length)
+        this.left = new AABBNode(leftHalf)
+        this.right = new AABBNode(rightHalf)
+        
+    }
+    
+    
+    //g can only be list of 2 boxes
+    boxAround2(firstBox, secondBox) {
+        
+        //call boxAround() and merge 2 boxes into one big box        
+        
+        const minX = Math.min(firstBox.v3_minPt.x, secondBox.v3_minPt.x);   //is this minX or just x
+        const minY = Math.min(firstBox.v3_minPt.y, secondBox.v3_minPt.y);
+        const minZ = Math.min(firstBox.v3_minPt.z, secondBox.v3_minPt.z);
+        const maxX = Math.max(firstBox.v3_minPt.x + firstBox.v3_dim.x, secondBox.v3_minPt.x + secondBox.v3_dim.x);
+        const maxY = Math.max(firstBox.v3_minPt.y + firstBox.v3_dim.y, secondBox.v3_minPt.y + secondBox.v3_dim.y);
+        const maxZ = Math.max(firstBox.v3_minPt.z + firstBox.v3_dim.z, secondBox.v3_minPt.z + secondBox.v3_dim.z);
+        
+         const ret = {
+            s_type: 'box',
+            v3_minPt: new Vector3(minX, minY, minZ),
+            v3_dim: new Vector3(maxX-minX, maxY-minY, maxZ-minZ),
+        };
+        
+        return ret
+        
+    }
+    
+    boxAround(g) {
+        /*
+        Return box around the given geometry.
+        */
+        if (g.s_type === 'sphere') {
+            return this.boxSphere(g);
+        }
+        else if (g.s_type === 'sheet') {
+            return this.boxSheet(g);
+        }
+        else if (g.s_type === 'box') {
+            return g;
+        }
+        else if (g.s_type === 'cylinder') {
+            return this.boxCylinder(g);
+        }
+        else if (g.s_type === 'cone') {
+            return this.boxCone(g);
+        }
+        else if (g.s_type === 'triangle') {
+            return this.boxTriangle(g);
+        }
+        else {
+            console.error("Shape of type " + g.s_type + " is not supported");
+        }
+        
+    }
+    
+    //pts is an array of points
+   boxPoints(pts) {
+        /*
+        Helper function: return box containing the given points
+        */
+        let [minX, minY, minZ] = [Infinity, Infinity, Infinity];
+        let [maxX, maxY, maxZ] = [-Infinity, -Infinity, -Infinity];
+        for (const p of pts) {
+            minX = Math.min(minX, p.x);
+            minY = Math.min(minY, p.y);
+            minZ = Math.min(minZ, p.z);
+            maxX = Math.max(maxX, p.x);
+            maxY = Math.max(maxY, p.y);
+            maxZ = Math.max(maxZ, p.z);
+        }
+        const ret = {
+            s_type: 'box',
+            v3_minPt: new Vector3(minX, minY, minZ),
+            v3_dim: new Vector3(maxX-minX, maxY-minY, maxZ-minZ),
+        };
+        return ret;
+    }
+    boxSheet(g) {
+        /*
+        Return box around the given geometry, a sheet.
+        */
+        return this.boxPoints([g.v3_pt0, g.v3_pt1, g.v3_pt2, vectorSum(g.v3_pt2, vectorDifference(g.v3_pt0, g.v3_pt1))]);
+    }
+    
+    boxCylinder(g) {
+                /*
+        Return box around the given geometry, a cylinder.
+        */
+        const ret = {
+            s_type: 'box',
+            v3_minPt: vectorSum(g.v3_center, new Vector3(-g.f_radius, -g.f_height/2, -g.f_radius)),
+            v3_dim: new Vector3(2*g.f_radius, g.f_height, 2*g.f_radius),
+        };
+        return ret;
+        
+    }
+    
+    boxSphere(g) {
+        const ret = {
+            s_type: 'box',
+            v3_minPt: vectorDifference(g.v3_center, g.f_radius),
+            v3_dim: new Vector3(2*g.f_radius, 2*g.f_radius, 2*g.f_radius)
+        };
+        return ret;
+        
+    }
+    
+    boxTriangle(g) {
+        
+        /*
+        Return box around the given geometry, a triangle.
+        */        
+        const minX = Math.min(g.v3_pt0.x, g.v3_pt1.x, g.v3_pt2.x);
+        const minY = Math.min(g.v3_pt0.y, g.v3_pt1.y, g.v3_pt2.y);
+        const minZ = Math.min(g.v3_pt0.z, g.v3_pt1.z, g.v3_pt2.z);
+        const maxX = Math.max(g.v3_pt0.x, g.v3_pt1.x, g.v3_pt2.x);
+        const maxY = Math.max(g.v3_pt0.y, g.v3_pt1.y, g.v3_pt2.y);
+        const maxZ = Math.max(g.v3_pt0.z, g.v3_pt1.z, g.v3_pt2.z);
+
+        const ret = {
+            s_type: 'box',
+            v3_minPt: new Vector3(minX, minY, minZ),
+            v3_dim: new Vector3(maxX-minX, maxY-minY, maxZ-minZ),
+        };
+        return ret;
+        
+    }
+    
+    boxCone(g) {
+        console.log(g)
+        
+        const ret = {
+            s_type: 'box',
+            v3_minPt: vectorSum(g.v3_center, new Vector3(-g.f_radius, -g.f_height/2, -g.f_radius)),
+            v3_dim: new Vector3(2*g.f_radius, g.f_height, 2*g.f_radius),
+        };
+        return ret;
+        
+    }
+    
+    
 }
